281f2a2122f3391e58b7f5df64846d860ae01731
Rashmi0210 <114385788+Rashmi0210@users.noreply.github.com>
2023-03-14 20:41:05 +0530

    Update connect.php

--- connect.php
+++ connect.php
@@ -4,7 +4,9 @@ $server = "shobhaserver.mysql.database.azure.com";
 $userid ="admin123";
 $Password = "shobha@123456789";
 $myDB = "states";
-$con = mysqli_connect($server,$userid,$Password,$myDB);
+$con = mysqli_init();
+mysqli_ssl_set($con,NULL,NULL, "{path to CA cert}", NULL, NULL);
+mysqli_real_connect($conn, "shobhaserver.mysql.database.azure.com", "admin123", "shobha@123456789", "{your_database}", 3306, MYSQLI_CLIENT_SSL);
 if (mysqli_connect_errno()) {
 echo "Failed to connect to MySQL: " . mysqli_connect_error();
 }
