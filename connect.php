e1ef7406806a2a3b6e1a279fadb6ad1c04304796
Rashmi0210 <114385788+Rashmi0210@users.noreply.github.com>
2023-03-14 20:43:53 +0530

    Update connect.php

--- connect.php
+++ connect.php
@@ -4,8 +4,7 @@ $server = "shobhaserver.mysql.database.azure.com";
 $userid ="admin123";
 $Password = "shobha@123456789";
 $myDB = "states";
-$con = mysqli_init();
-mysqli_real_connect($con, "shobhaserver.mysql.database.azure.com", "admin123", "shobha@123456789", "{states}", 3306);
+$con = mysqli_connect($server,$userid,$Password,$myDB);
 if (mysqli_connect_errno()) {
 echo "Failed to connect to MySQL: " . mysqli_connect_error();
 }
